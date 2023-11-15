b25067d05936f555115d20e64198badbcf4f5ace
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2023-11-15 21:39:07 +0100

    Add project files.

---
+++ TCP_Socket_client_server_aplication_console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TCP_Socket_client_server_aplication_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
