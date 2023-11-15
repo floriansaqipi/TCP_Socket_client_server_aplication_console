3e5b2cfed98c6a939ee67a815f30c044da2fb0a2
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2023-11-15 21:46:44 +0100

    initial-TODOS

--- TCP_Socket_client_server_aplication_console/Program.cs
+++ TCP_Socket_client_server_aplication_console/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //TODO: Implement server CLI
+            //TODO: Implement client CLI
+            //TODO: Implement basic message receive function server-side
+            //TODO: Implement basic message sending function client-side
+            //TODO: Add privileges to specified user
+            //TODO: Add read,write,execut
         }
     }
 }
